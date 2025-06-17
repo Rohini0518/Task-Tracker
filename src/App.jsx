58c9e7ec32c2c820c4868d82b07b70de289910e8
Rohini0518 <rohinipolina0518@gmail.com>
2025-06-17 16:55:30 +0530

    added backend render url to tasklist api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import Header from './components/Header';
-import InfoChips from './components/InfoChips';
-import TaskCard from './components/TaskCard';
+import Header from "./components/Header";
+import InfoChips from "./components/InfoChips";
+import TaskCard from "./components/TaskCard";
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <InfoChips/>
-<TaskCard/>
+      <Header />
+      <InfoChips />
+      <TaskCard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
